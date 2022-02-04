9060caa2cbdb93ee8ffe46fad1116cff50761c2a
ficus <fkszkksz@users.noreply.github.com>
2022-02-04 12:02:21 -0800

    Added patch for bullet parry

---
+++ AddPointsOnBulletParry.cs
@@ -0,0 +1,28 @@
+﻿extern alias Akane;
+extern alias AkaneFP;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+
+using HarmonyLib;
+using Unity;
+using UnityEngine;
+
+namespace AkaneParryAnytime
+{
+    [HarmonyPatch(typeof(Akane::Player), "Parry")]
+    [HarmonyPatch(new Type[] { typeof(Rigidbody) })]
+    public class BulletParryPatch
+    {
+
+        static void Postfix(Rigidbody OtherRb)
+        {
+            Akane::Player player = GlobalVars.player;
+            player.AddAmmo();
+            player.AddAdrenalinePoint();
+        }
+    }
+}
